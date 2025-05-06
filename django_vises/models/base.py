c97486e2c5bbff57c201efeb685e815d00babd60
Rex Zhang <rex.zhang@gmail.com>
2025-05-06 22:16:56 +0800

    Update database model

--- django_vises/models/base.py
+++ django_vises/models/base.py
@@ -1,62 +1,88 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Any
 from uuid import uuid4
 
-import arrow
 from django.db import models
 from django.utils.timezone import now
 
 
-class ModelBaseObjectNoId(models.Model):
+class RecordManager(models.Manager):
+    def get_last_x_hour_queryset(self, hours):
+        return (
+            super()
+            .get_queryset()
+            .filter(created_time__gte=now() - timedelta(hours=hours))
+        )
+
+    def get_last_x_day_queryset(self, days):
+        return (
+            super()
+            .get_queryset()
+            .filter(created_time__gte=now() - timedelta(days=days))
+        )
+
+
+class RecordAbcWithoutIdAbc(models.Model):
+    """基本数据库模型 - 日志型 - 无 id
+    创建时间，无更新需求
+    """
+
+    # Django 5.2 开始支持联合主键,继承后添加主键字段
+
+    # 记录创建时间
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    objects = RecordManager()
+
+    class Meta:
+        abstract = True
+
+
+class RecordAbc(RecordAbcWithoutIdAbc):
+    """基本数据库模型 - 记录型
+    创建时间，无更新需求
+    """
+
+    uuid = models.UUIDField(
+        primary_key=True, unique=True, default=uuid4, editable=False
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"{self.uuid}"
+
+
+class ObjectWithoutIdAbc(models.Model):
     """基本数据库模型 - 对象型 - 无 id
     创建时间，最后更新时间, 需要继承者自行实现 __str__()
     """
 
     # 记录创建时间
-    time_created = models.DateTimeField(
-        auto_now_add=True,
-    )
+    created_time = models.DateTimeField(auto_now_add=True)
 
     # 记录最后更新时间
-    time_last_updated = models.DateTimeField(
-        auto_now=True,
-    )
+    updated_time = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
 
-class ModelBaseObject(ModelBaseObjectNoId):
+class ObjectAbc(ObjectWithoutIdAbc):
     """基本数据库模型 - 对象型
     创建时间，最后更新时间
     """
 
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid4, editable=False)
-
-    class Meta:
-        abstract = True
-
-    def __str__(self):
-        return f"{self.id}"
-
-
-class ModelBaseRecord(models.Model):
-    """基本数据库模型 - 记录／日志型
-    创建时间，无更新需求
-    """
-
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid4, editable=False)
-
-    # 记录创建时间
-    time_created = models.DateTimeField(
-        auto_now_add=True,
+    uuid = models.UUIDField(
+        primary_key=True, unique=True, default=uuid4, editable=False
     )
 
     class Meta:
         abstract = True
 
     def __str__(self):
-        return f"{self.id}"
+        return f"{self.uuid}"
 
 
 class KeyValueManager(models.Manager):
@@ -67,23 +93,19 @@ class KeyValueManager(models.Manager):
         return
 
     def get_object(self, key: str, group: str | None = None):
-        return super().get_queryset().filter(group=group, key=key).first()
+        return super().get_queryset().filter(group=group, key=key).get()
 
     def get_value(self, key: str, group: str | None = None) -> Any:
         obj = self.get_object(key=key, group=group)
-        if obj is None:
-            raise
 
         return obj.value
 
-    def get_value_and_update_time(
+    def get_value_and_update_time(  # TODO:这个应该被废弃,使用 get_object 替代
         self, key: str, group: str | None = None
     ) -> tuple[list, datetime | None]:
         obj = self.get_object(key=key, group=group)
-        if obj is None:
-            return [], None
 
-        return obj.value, obj.time_last_updated
+        return obj.value, obj.updated_time
 
     def get_values_by_group(self, group: str) -> dict:
         return dict(
@@ -93,10 +115,13 @@ class KeyValueManager(models.Manager):
         )
 
 
-class ModelBaseKeyValue(ModelBaseObject):
+class KeyValueAbc(ObjectWithoutIdAbc):
     """基本数据库模型 - 键值对型
     创建时间，最后更新时间
     """
+
+    # TODO:拆分为两个基类,分别包含和不包含 group, 添加联合主键
+    # TODO:替代方案: 使用 prefix 概念替代 group 概念
 
     # group - 可选的一级分组
     group = models.TextField(null=True)
@@ -105,7 +130,7 @@ class ModelBaseKeyValue(ModelBaseObject):
     # value
     value = models.JSONField()
 
-    kv_objects = KeyValueManager()
+    objects = KeyValueManager()
 
     class Meta:
         abstract = True
@@ -115,68 +140,7 @@ class ModelBaseKeyValue(ModelBaseObject):
         ]
 
     def __str__(self):
-        return f"{self.group}:{self.key}:{self.value}"
-
-
-class BaseAbstract(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid4)
-
-    time_created = models.DateTimeField(
-        auto_now_add=True,
-        db_index=True,
-    )
-
-    class Meta:
-        abstract = True
-
-    def __str__(self):
-        return f"{self.id}"
-
-
-class BaseObjectAbstract(BaseAbstract):
-    time_last_updated = models.DateTimeField(
-        auto_now=True,
-        db_index=True,
-    )
-
-    class Meta:
-        abstract = True
-
-
-class HistoryManager(models.Manager):
-    def get_last_x_hour_queryset(self, hours):
-        return (
-            super()
-            .get_queryset()
-            .filter(
-                datetime__gte=arrow.now()
-                .replace(minute=0, second=0, microsecond=0)
-                .shift(hours=-hours)
-                .datetime
-            )
-        )
-
-    def get_last_x_day_queryset(self, days):
-        return (
-            super()
-            .get_queryset()
-            .filter(
-                datetime__gte=arrow.now()
-                .replace(hour=0, minute=0, second=0, microsecond=0)
-                .shift(days=-days)
-                .datetime
-            )
-        )
-
-
-class HistoryAbstract(models.Model):
-    # Django 不支持多列主键
-    # https://docs.djangoproject.com/zh-hans/4.0/faq/models/#do-django-models-support-multiple-column-primary-keys
-    # 让 Django 自行创建用于放置 pk 的 id 字段
-    # 具体创建为 AutoField/BigAutoField， 取决于 XyzConfig.default_auto_field 设置
-    # id = models.BigAutoField(primary_key=True)
-
-    datetime = models.DateTimeField(default=now)
-
-    class Meta:
-        abstract = True
+        if self.group is None:
+            return f"{self.key}:{self.value}"
+        else:
+            return f"{self.group}.{self.key}:{self.value}"
