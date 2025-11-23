a5a977fa94f99c9f0be19f3187cdf56b641684cd
Rex Zhang <rex.zhang@gmail.com>
2025-11-23 16:10:24 +0800

    refactor: restructure redis dataset classes and add tests

--- django_vises/db/data_set_redis.py
+++ django_vises/db/data_set_redis.py
@@ -8,20 +8,34 @@ class DataSetRedisAbs:
         self.conn = redis.from_url(url=url)
         self.name = name
 
-    def add(self, item: bytes):
+    def _add(self, item: bytes):
         self.conn.sadd(self.name, item)
 
     def pop(self):
         raise
 
-    def remove(self, item: bytes):
+    def _remove(self, item: bytes):
         self.conn.srem(self.name, item)
 
-    def all(self) -> set[bytes]:
-        return self.conn.smembers(self.name)
+    def _all(self) -> set[bytes]:
+        # 异步模式下，返回的是 collections.abc.Awaitable[set[bytes]]
+        return self.conn.smembers(self.name)  # type: ignore
 
-    def purge(self):
+    def clear(self):
         return self.conn.delete(self.name)
+
+
+class DataSetStr(DataSetRedisAbs):
+    def add(self, item: str):
+
+        super()._add(item.encode("utf-8"))
+
+    def remove(self, item: str):
+
+        super()._remove(item.encode("utf-8"))
+
+    def all(self) -> list[str]:
+        return [item.decode("utf-8") for item in super()._all()]
 
 
 class DataSetInt(DataSetRedisAbs):
@@ -30,20 +44,16 @@ class DataSetInt(DataSetRedisAbs):
     需要指定转换的长度，这导致通用型变差，不如统一转换成 str 再转换为 bytes
     """
 
-    def add(self, item: int | str):
-        if isinstance(item, int):
-            item = str(item)
+    def add(self, item: int):
 
-        super().add(bytes(item, "utf-8"))
+        super()._add(str(item).encode("utf-8"))
 
-    def remove(self, item: int | str):
-        if isinstance(item, int):
-            item = str(item)
+    def remove(self, item: int):
 
-        super().remove(bytes(item, "utf-8"))
+        super()._remove(str(item).encode("utf-8"))
 
-    def all(self) -> list[str]:
-        return [str(item, "utf-8") for item in super().all()]
+    def all(self) -> list[int]:
+        return [int(item.decode("utf-8")) for item in super()._all()]
 
 
 class DataSetUUID(DataSetRedisAbs):
@@ -51,35 +61,13 @@ class DataSetUUID(DataSetRedisAbs):
         if isinstance(item, str):
             item = UUID(item)
 
-        super().add(item.bytes)
+        super()._add(item.bytes)
 
     def remove(self, item: UUID | str):
         if isinstance(item, str):
             item = UUID(item)
 
-        super().remove(item.bytes)
+        super()._remove(item.bytes)
 
     def all(self) -> list[UUID]:
-        return [UUID(bytes=item) for item in super().all()]
-
-
-if __name__ == "__main__":
-    r = DataSetInt(url="redis://localhost:6379/2", name="test")
-    r.purge()
-    r.add(1)
-    print(r.all())
-    r.add("2")
-    print(r.all())
-    r.remove("2")
-    print(r.all())
-    r.remove(1)
-    print(r.all())
-
-    from uuid import uuid4
-
-    r = DataSetUUID(url="redis://localhost:6379/2", name="test")
-    r.purge()
-    r.add(uuid4())
-    print(r.all())
-    r.add(uuid4().hex)
-    print(r.all())
+        return [UUID(bytes=item) for item in super()._all()]
